85996689b3b223aa9560e848d8f68aac2cc8de57
Radu H <52208792+Radoogle@users.noreply.github.com>
2020-02-24 18:57:52 +0200

    temp save

--- javas.js
+++ javas.js
@@ -1,9 +1,25 @@
+var inputNote = document.getElementById("inputnote"),
+    list = document.getElementById("savednotes");
+
+for (let i = 0; i < localStorage.length; i++) {
+    let li = document.createElement("li"),
+        key = localStorage.key(i);
+    
+    li.innerHTML = localStorage.getItem(key);
+    list.appendChild(li);
+}
+
+
+
 function saveNote () {
-    var inputNote = document.getElementById("inputnote"),
-        noteText = inputNote.value,
-        list = document.getElementById("savednotes"),
-        li = document.createElement("li");
+    let li = document.createElement("li"),
+        noteText = inputNote.value;
+    
     inputNote.value = "";
     li.innerHTML = noteText;
     list.appendChild(li);
+    const key = localStorage.length;
+    const value = noteText;
+    localStorage.setItem(key, value);
+    
 }
